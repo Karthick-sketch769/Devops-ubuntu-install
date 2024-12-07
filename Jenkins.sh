1cd92d5fc75b5395f114fc6a1b3473653dfcecbc
Karthick-sketch769 <karthicksethu189@gmail.com>
2024-12-07 16:18:11 +0530

    Update Jenkins.sh

--- Jenkins.sh
+++ Jenkins.sh
@@ -4,4 +4,4 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
 https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
 /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt update
-sudo apt install jenkins -
+sudo apt install jenkins -y
